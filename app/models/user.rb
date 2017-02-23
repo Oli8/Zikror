bf5b9f393e856013a5b5a02dba483ff6566f15ff
Olivier Crete <olivier.crete@orange.com>
2017-02-23 14:56:17 +0100

    singup validation rules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+
+	validates :username,
+				presence: true,
+				length: {minimum: 3, maximum: 20},
+				format: {with: /\A[A-Za-z][A-Za-z\d_]*\z/, message: 'must starts with a letter and includes only letters and digits or underscore'}
+
+	validates :mail,
+				presence: true,
+				format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: 'must be valid'}
+
+	validates :password,
+				length: {minimum: 6, maximum: 50},
+				format: {with: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\A/, message: 'must includes at least one lower case letter, one upper case letter and one numeric digit.'}
 end
