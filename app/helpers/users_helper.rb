edba64ba000c5d81126bcb63183f9e4e96479c79
Olivier Crete <olivier.crete@orange.com>
2017-02-24 12:04:13 +0100

    jquery

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
 	def gravatar(user)
-    	image_tag('https://secure.gravatar.com/avatar/' + Digest::MD5::hexdigest(user.mail.downcase), alt: user.username, class: "gravatar")
+		image_tag('https://secure.gravatar.com/avatar/' + Digest::MD5::hexdigest(user.mail.downcase), alt: user.username, class: 'gravatar')
 	end
 
 end
