2784132a425857d9ad7453bdf20b2ee865be3e8b
Olivier Crete <olivier.crete@orange.com>
2017-02-28 16:10:00 +0100

    login and sign up on the home page

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,23 +1,21 @@
 class SessionController < ApplicationController
   
   def new
+	@user = User.new
   end
 
   def create
   	user = User.find_by(username: params[:session][:username])
   	if user && user.authenticate(params[:session][:password])
-  		puts 'ok'
   		log_in user
   		redirect_to user
   	else
-  		puts 'nop'
   		flash.now[:notice] = 'Invalid parameters'
   		render 'new'
   	end
   end
 
   def destroy
-	puts 'logout'
 	log_out
 	redirect_to root_url
   end
