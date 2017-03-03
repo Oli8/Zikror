5405ce81cf8a6256f3d693c1e5864b98ece5d63c
Olivier Crete <olivier.crete@orange.com>
2017-03-03 18:08:45 +0100

    list artist on profil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @artists = @user.songs.group(:artist).count.sort.reverse
   end
 
   # GET /users/1/edit
