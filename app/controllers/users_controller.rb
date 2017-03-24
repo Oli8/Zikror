1fed469ef2ca606ffeec82a21b3521d57a9c3783
Olivier Crete <olivier.crete@orange.com>
2017-03-24 16:26:31 +0100

    users show with username route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -75,7 +75,13 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      if params[:id].to_i != 0
+        @user = User.find(params[:id])
+      else
+        @user = User.find_by(username: params[:id])
+      end
+
+      render404 if @user.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
