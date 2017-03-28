255bbb3c9a13959478e45523c61d2e8ab4a85080
Olivier Crete <olivier.crete@orange.com>
2017-03-28 16:55:42 +0200

    relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,17 @@ class User < ApplicationRecord
 	has_many :playlists, dependent: :destroy
 	has_many :favorite_songs, dependent: :destroy
 
+	has_many :active_relationships, class_name: "Relationship",
+									foreign_key: "follower_id",
+									dependent: :destroy
+
+	has_many :passive_relationships, class_name: "Relationship",
+									foreign_key: "followed_id",
+									dependent: :destroy
+
+	has_many :following, through: :active_relationships, source: :followed
+	has_many :followers, through: :passive_relationships, source: :follower
+
 	validates :username,
 				presence: {message: 'Veuillez saisir un nom d\'utilisateur'},
 				uniqueness: {message: 'Ce nom d\'utilisateur existe déjà'},
@@ -20,5 +31,19 @@ class User < ApplicationRecord
 				format: {with: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\A/, message: 'Votre mot de passe doit comprendre au moins une lettre minuscule, une lettre majuscule et un chiffre numérique'}
 				validates_length_of :password, within: 3..20, too_long: 'Veuillez saisir un nom entre 6 et 50 caractère', too_short: 'Veuillez saisir un mot de passe entre 6 et 50 caractère'
 
+	# Follows a user.
+	def follow(other_user)
+		self.following << other_user
+	end
+
+	# Unfollows a user.
+	def unfollow(other_user)
+		self.following.delete(other_user)
+	end
+
+	# Returns true if the current user is following the other user.
+	def follow?(other_user)
+		self.following.include?(other_user)
+	end
 end
 	
