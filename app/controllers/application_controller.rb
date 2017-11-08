ab39d45c2a365aded0708f36e8136d1a8da51a65
Olivier Crété <ocrete@sfrpims.fr>
2017-11-08 14:25:52 +0100

    Title handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionHelper
+  before_filter :set_constants
+
+	def set_constants
+		@slogan = 'Let the Music Flow'
+	end
 
   def render404
     render file: 'public/404', status: 404
